2feaccaa26d459a38153887d5e7b97732073f7f8
40349311 <mhiggins26@qub.ac.uk>
2022-03-01 12:55:06 +0000

    Revert "Merge branch 'revert-fe16437e' into 'main'"
    This reverts merge request !13

--- Team Project ArtemisLite/src/g11ArtemisLite/Dice.java
+++ Team Project ArtemisLite/src/g11ArtemisLite/Dice.java
@@ -1,31 +1,29 @@
 package g11ArtemisLite;
 
+/**
+ * Dice class, one dice which returns a random number between 1-6
+ * 
+ * @author Robbie
+ *
+ */
+
+
 import java.util.Random;
 
 public class Dice {
-
+	
 	private int numOfSides;
-		
+
 	/**
 	 * @param numOfSides
 	 */
 	public Dice(int numOfSides) {
 		this.numOfSides = numOfSides;
 	}
-	
-	public int getNumOfSides() {
-		return numOfSides;
-	}
-
-	public void setNumOfSides(int numOfSides) {
-		this.numOfSides = numOfSides;
-	}
-
-	// generates random number+1 within specified "numOfSides" range
 
 	public int roll() {
-		Random ran = new Random();
-		return ran.nextInt(numOfSides)+1;
+		return (int) (Math.random() * numOfSides + 1);
 	}
 
 }
+
---
+++ Team Project ArtemisLite/src/g11ArtemisLite/GameManager.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package g11ArtemisLite;
+
+/**
+ * @author crclarke
+ *
+ */
+public class GameManager {
+	
+	private Game game;
+
+	/**
+	 * 
+	 */
+	public GameManager() {
+		this.game = new Game();
+	}
+
+}
