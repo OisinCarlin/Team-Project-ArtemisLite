a9349ccf88f75554c851b20e079afda0d7a6a27f
40314471 <cclarke60@qub.ac.uk>
2022-03-03 20:04:21 +0000

    Added test folder

---
+++ Team Project ArtemisLite/test/g11ArtemisLite/GameTest.java
@@ -0,0 +1,55 @@
+package g11ArtemisLite;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		
+	}
+
+	@Test
+	void testGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testStart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDisplayIntroMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReturnDevelopableElements() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNoDevelopmentsToMakeChecker() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDevelopElement() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDevelopmentMenu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTradeElementMenu() {
+		fail("Not yet implemented");
+	}
+
+}
